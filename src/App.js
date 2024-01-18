4cddfb912171a6ecf24825bf06edba4163cb5e13
MadheshKumarVJ <vjmadheshtestpress.in>
2024-01-18 21:06:25 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import VideoJSPlayerComponent from "./video-js/videojs";
+
+function App() {
+  return (
+    <div className="App">
+      <VideoJSPlayerComponent/>
+    </div>
+  );
+}
+
+export default App;
